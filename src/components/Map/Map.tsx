ae4970838bc024b8967da00f1271569657cc24e8
MariuszRudnik <86014393+MariuszRudnik@users.noreply.github.com>
2022-05-21 11:25:14 +0200

    i'm created map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import '../../utils/fix-map-icon'
+import 'leaflet/dist/leaflet.css';
 import './map.css';
 
 export const Map = ()=>{
     return(
         <div className="map">
-
+            <MapContainer center={[50.2657152, 18.9945008]} zoom={15}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={[50.2657152, 18.9945008]}>
+                    <Popup>
+                        <h2>It.Focus</h2>
+                        <p>Firma co programuje</p>
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </div>
     )
 }
