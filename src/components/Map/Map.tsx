ceab4eb0581d8989806c1977d9b0a017de24126d
MariuszRudnik <86014393+MariuszRudnik@users.noreply.github.com>
2022-05-24 20:15:55 +0200

    added Context

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useContext, useEffect} from "react";
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import '../../utils/fix-map-icon'
 import 'leaflet/dist/leaflet.css';
 import './map.css';
+import {SearchContext} from "../../contexts/search.context";
+
 
 export const Map = ()=>{
+    const {search} = useContext(SearchContext);
+
+    useEffect(()=>{
+        console.log('sss')
+    },[search])
     return(
         <div className="map">
+                <h1>Serch for {search}</h1>
             <MapContainer center={[50.2657152, 18.9945008]} zoom={15}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
